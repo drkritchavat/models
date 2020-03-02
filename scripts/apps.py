d66c5d1dabdbe90659f2270cd855e36f61b2400a
drkritchavat <53193193+drkritchavat@users.noreply.github.com>
2020-03-02 11:21:07 +0700

    add simple weekly plotly epidemic curve

--- scripts/apps.py
+++ scripts/apps.py
@@ -5,6 +5,8 @@ import numpy as np
 from models import seiqr_solver,to_beta, seir_Rn_solver
 import matplotlib.pyplot as plt
 import json
+import plotly.express as px
+import plotly
 
 def seiqr(model,
           y0=[1000000,0,1,0,0,0,0], # [S,E,I,Q,R,CI,CQ]
@@ -90,6 +92,40 @@ def seiqrapi():
                     'ResultsDays':resultsDays,
                     'ResultsDaysCols':resultsCols,
                     'NewCaseWeeks':resultsWeeks})
+
+@app.route('/seiqr_d3')  
+def seiqr_d3():
+    plot = int(request.args.get('plot',1))
+    y0 = json.loads(request.args.get('y0','[1000000,0,1,0,0,0,0]'))
+    t = int(request.args.get('t',365))
+    R0_I = float(request.args.get('R0_I',2.2))
+    R0_Q = float(request.args.get('R0_Q',0))
+    epsilon_E = float(request.args.get('epsilon_E',0.01))
+    epsilon_I = float(request.args.get('epsilon_I',0.2))
+    dInc = float(request.args.get('dInc',5.2))
+    dInf = float(request.args.get('dInf',2.3))
+
+    model_result_days, model_result_weeks = seiqr(model='seiqr1',
+                                                  y0=y0,
+                                                  #N=N,
+                                                  t=t,
+                                                  #I0=I0,
+                                                  R0_I=R0_I,
+                                                  R0_Q=R0_Q,
+                                                  epsilon_I=epsilon_I,
+                                                  epsilon_E=epsilon_E,
+                                                  dInc=dInc,
+                                                  dInf=dInf)
+    model_result_weeks = pd.DataFrame(model_result_weeks.fillna(0)).reset_index()
+    model_result_weeks.columns = ['week','cases']
+    barchart = px.bar(model_result_weeks, y='cases',x='week')
+
+    barchartweek = json.dumps(barchart, cls=plotly.utils.PlotlyJSONEncoder)
+    if plot == 1:
+        return render_template('index.html', plot=barchartweek)
+    else:
+        return jsonify(barchartweek)
+
 @app.route('/seir_Rn')  
 def seir_Rn():
     y0 = json.loads(request.args.get('y0','[1000000,0,1,0,0]'))
